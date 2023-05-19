33399a8ae7c87a6bb604f6b612f30d79b6466aba
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-05-20 01:08:58 +0530

    Add fn shell_prompt and shell_input

--- ls-with-cache/src/main.rs
+++ ls-with-cache/src/main.rs
@@ -1,3 +1,26 @@
+#![allow(dead_code)]
+#![allow(unused_parens)]
+#![allow(unused_must_use)]
+
+use std::io::{stdout, Write};
+
+fn shell_prompt() {
+    let user = std::env::var("USER").expect("Unable to find USER env var");
+    print!("> {} $ ", user);
+}
+
+fn shell_input() {
+    let mut input = String::new();
+    let _ = stdout().flush();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Error: Unable to take input from user");
+}
+
 fn main() {
-    println!("Hello, world!");
+    print!("[!] Running Rust shell. \n\n");
+    loop {
+        shell_prompt();
+        shell_input();
+    }
 }
