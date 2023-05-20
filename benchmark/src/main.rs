28155493c55673d9e7c8ac39ca35f24f7ad891d4
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-05-20 21:44:42 +0530

    Complete basic benchmark

--- benchmark/src/main.rs
+++ benchmark/src/main.rs
@@ -1,3 +1,24 @@
+use std::time::SystemTime;
+
+fn benchmark_function() {
+    let mut _c: u32 = 0;
+    for _i in 0..100000 {
+        _c += 1;
+    }
+}
+
+fn do_benchmark(times: u64) {
+    println!("[!] Running benchmark {} times", times);
+    let now = SystemTime::now();
+    for _i in 0..times {
+        benchmark_function();
+    }
+
+    let end = now
+        .elapsed()
+        .expect("Error: Unable to record the benchmark");
+    println!("Time taken: {}", end.as_secs_f32());
+}
 fn main() {
-    println!("Run Benchmark");
+    do_benchmark(1);
 }
