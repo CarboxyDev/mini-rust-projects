ab61a929d58e5cbbcba4fee204706453306f65d9
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-05-20 22:01:24 +0530

    Add average benchmark time

--- benchmark/src/main.rs
+++ benchmark/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused_parens)]
 use std::time::SystemTime;
 
 fn benchmark_function() {
@@ -7,8 +8,7 @@ fn benchmark_function() {
     }
 }
 
-fn do_benchmark(times: u64) {
-    println!("[!] Running benchmark {} times", times);
+fn do_benchmark(times: u64) -> f32 {
     let now = SystemTime::now();
     for _i in 0..times {
         benchmark_function();
@@ -16,13 +16,28 @@ fn do_benchmark(times: u64) {
 
     let end = now
         .elapsed()
-        .expect("Error: Unable to record the benchmark");
+        .expect("Error: Unable to record the benchmark. The benchmark result may be inaccurate.");
     println!("Time taken: {}", end.as_secs_f32());
+    return end.as_secs_f32();
 }
 fn main() {
-    const BENCHMARK_COUNT: u64 = 5; // Number of times the benchmark should be timed
+    const BENCHMARK_COUNT: u64 = 10; // Number of times the benchmark should be timed
     const EACH_BENCHMARK_TIMES: u64 = 1; // Number of times the benchmark function should be ran in each timed benchmark
+    let mut all_benchmarks: Vec<f32> = vec![];
+    println!("[!] Doing the Benchmark {} times", BENCHMARK_COUNT);
+    println!(
+        "[!] Running the benchmark function in each Benchmark {} times",
+        EACH_BENCHMARK_TIMES
+    );
     for _i in 0..BENCHMARK_COUNT {
-        do_benchmark(EACH_BENCHMARK_TIMES);
+        let benchmark_time: f32 = do_benchmark(EACH_BENCHMARK_TIMES);
+        all_benchmarks.push(benchmark_time);
+    }
+
+    if (BENCHMARK_COUNT > 1) {
+        let avg_benchmark_time: f32 = all_benchmarks.iter().sum::<f32>() / (BENCHMARK_COUNT as f32);
+        println!("---------------------------------------------------------------");
+        println!("Average benchmark time: {}", avg_benchmark_time);
+        println!("---------------------------------------------------------------");
     }
 }
