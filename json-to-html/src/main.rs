1988bd4f6e61f4299e8ad619a2ee5caa2965bb35
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-05-20 18:28:27 +0530

    Add working JSON reader

--- json-to-html/src/main.rs
+++ json-to-html/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let file = std::fs::File::open("html.json").expect("Error: Unable to open file");
+    let json: serde_json::Value =
+        serde_json::from_reader(file).expect("Error: Unable to read the JSON file");
+
+    let name = json.get("name").unwrap();
+    println!("{}", name);
 }
